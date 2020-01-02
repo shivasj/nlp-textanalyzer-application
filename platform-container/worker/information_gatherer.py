dd57e401398f3471030a0ebb2c9e2376da0f4732
shivasj <shivasj@gmail.com>
2020-01-02 10:01:02 -0500

    Added base code for information gatherer

---
+++ platform-container/worker/information_gatherer.py
@@ -0,0 +1,140 @@
+import os
+import requests
+import hashlib
+import csv,re,json,time,sys
+from dateutil import parser
+from bs4 import BeautifulSoup
+from bs4 import Comment
+from datetime import datetime
+
+news_source = [
+    {'name': 'npr',
+     'URL': 'https://www.npr.org/sections/politics/',
+     'Checker': 'https://www.npr.org/',
+     'LINKS': {'div': 'h2', 'class': 'title'},
+     'TIME': {'div': 'span', 'class': 'date'},
+     'TITLE': {'div': 'h1', 'class': None},
+     'CONTENT': {'div': 'div', 'class': 'storytext storylocation linkLocation', 'p': 'p', 'pc': None}
+     }
+]
+
+
+def generate_hash(string):
+    return hashlib.sha256(string.encode('utf-8')).hexdigest()
+
+
+def find_links(dct):
+    ''' This Function is used to find the links in a given site '''
+    links = []
+    # Extract blog informations
+    url = dct['URL']
+    details = dct['LINKS']
+    Checker = dct['Checker']
+    # Use headers to pass the authentication
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    # Request the page
+    page = requests.get(url, headers=headers)
+    # Parsing
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # Extracting the blog links
+    for data in soup.find_all(details['div'], class_=details['class']):
+        for a in data.find_all('a'):
+            link = a.get('href')
+            # This exception for CNN blog only
+            if dct['Checker'] == 'https://edition.cnn.com/':
+                link = 'https://edition.cnn.com{}'.format(link)
+            # This for all blogs
+            if link != None and link not in links and link.startswith(Checker):
+                links.append(link)
+    # Links Found
+    print("FOUND {} LINKS IN THIS BLOG ...".format(len(links)))
+    return links
+
+
+def scrape_page(link, dct):
+    ''' This Function is used to extract blogs information and print it to the terminal'''
+
+    # Request the page
+    page = requests.get(r'{}'.format(link))
+
+    # HTML Parser
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    # Loading page information
+    time_details = dct['TIME'] # Article datetime
+    title_details = dct['TITLE'] # Article title
+    content_details = dct['CONTENT'] # Article content
+
+    # Get formated date
+    #date = re.search(r'\w* \d*, \d\d\d\d', time).group(0)
+
+    # Extract required information
+    time = soup.find(time_details['div'], class_=time_details['class']).text
+    title = soup.find(title_details['div'], class_=title_details['class']).text
+
+    # Get the article body content
+    content = ''
+    for data in soup.find_all(content_details['div'], class_=content_details['class']):
+        # if content_details['pc'] is None:
+        #     for p in data.find_all(content_details['p']):
+        #         if p.text not in text:
+        #             text.append(p.text)  # for getting text
+        # else:
+        #     for p in data.find_all(content_details['p'], class_=content_details['pc']):
+        #         if p.text not in text:
+        #             text.append(p.text)  # for getting text
+        # content = '\n'.join(text)
+
+        # Remove unwanted tags
+        [x.extract() for x in data.find_all('script')]
+        [x.extract() for x in data.find_all('style')]
+        [x.extract() for x in data.find_all('meta')]
+        [x.extract() for x in data.find_all('noscript')]
+        [x.extract() for x in data.find_all('div', class_="image")]
+        [x.extract() for x in data.find_all(text=lambda text: isinstance(text, Comment))]
+
+        # Extract content text
+        content = data.get_text()
+        content = content.replace('\r', ' ').replace('\n', ' ')
+        content = " ".join(content.split())
+
+    # Extracted data
+    article = {
+        "id": generate_hash(link),
+        "article_link": link,
+        "article_date": time,
+        "article_title": title,
+        "article_content": content
+    }
+
+    return article
+
+
+def gather_news_articles(dct):
+    """
+    This Function is used to Automate the process of news feed scrapping
+    :param dct:
+    :return:
+    """
+
+    # Find the links
+    links = find_links(dct)
+    # Find the data in each link
+    for link in links:
+        try:
+            # Scrap the page
+            article = scrape_page(link, dct)
+            print(article)
+            print('\n\n\n')
+
+            # Save the news article
+
+        except Exception as Error:
+            print(link)
+            print(Error)
+
+
+if __name__ == "__main__":
+    for source in news_source:
+        gather_news_articles(source)
