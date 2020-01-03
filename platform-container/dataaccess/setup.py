663a16a9fed3ff19418fe3101978fc3e373b23b0
shivasj <shivasj@gmail.com>
2020-01-03 09:12:31 -0500

    Added scripts to create table articles

--- platform-container/dataaccess/setup.py
+++ platform-container/dataaccess/setup.py
@@ -4,7 +4,22 @@ from dataaccess.session import database
 if __name__ == "__main__":
     print("Running database setup....")
 
+    cursor = database.cursor()
+
     # News
 
     # Articles Table
+    sql = """
+            CREATE TABLE IF NOT EXISTS articles(
+               id TEXT PRIMARY KEY NOT NULL,
+               source TEXT NOT NULL,
+               article_link TEXT NOT NULL,
+               article_date TEXT NOT NULL,
+               article_title TEXT NOT NULL,
+               article_content TEXT NOT NULL,
+               article_dts INTEGER NOT NULL
+            );
+        """
 
+    print(sql)
+    cursor.execute(sql)
