3eadb94a132ac9b736b72872cfad2c5aa60c2d24
shivasj <shivasj@gmail.com>
2020-01-06 09:03:59 -0500

    Added new params to articles api

--- platform-container/api/routers/articles.py
+++ platform-container/api/routers/articles.py
@@ -26,12 +26,32 @@ class ArticleDetails(BaseModel):
     response_model=List[ArticleDetails]
 )
 async def source_index(
-        last_n_hours: int = Query(None, description="The last 'n' hours to filter by"),
+        summarize: bool = Query(False, description="Summarize article or not"),
+        summarize_ratio: float = Query(None, description="Summarization ratio"),
+        summarize_word_count: int = Query(None, description="Summarization word count"),
+        day: int = Query(None, description="The day to filter by, 0 is today 1 is yesterday"),
+        hours: int = Query(None, description="The last 'n' hours to filter by"),
         page_number: int = Query(None, description="The page number to filter by"),
         page_size: int = Query(None, description="The page size to filter by")
 ):
+    if page_number is None:
+        page_number = 0
+
+    if summarize_ratio is None:
+        summarize_ratio = 0.3
+
+    if page_size is None:
+        page_size = 20
+
+    if day is None:
+        day = 0
+
     return await articles.browse(
-        last_n_hours=last_n_hours,
+        summarize=summarize,
+        summarize_ratio=summarize_ratio,
+        summarize_word_count=summarize_word_count,
+        day=day,
+        hours=hours,
         page_number=page_number,
         page_size=page_size
     )
